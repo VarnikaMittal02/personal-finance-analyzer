1abae9f5a3361674504dedac1420541a16ca2668
Varnika Mittal <varnikamittal0@gmail.com>
2025-03-18 21:04:56 +0530

    Initial commit

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,5 +1,20 @@
 import AccountsPageComponent from "@/components/accounts-page";
 
 export default function AccountsPage() {
-  return <AccountsPageComponent />;
+  return import AccountsPageComponent from "@/components/accounts-page";
+
+/**
+ * Accounts Page component.
+ * 
+ * This component renders the AccountsPageComponent.
+ * 
+ * @returns {JSX.Element} The AccountsPageComponent.
+ */
+export default function AccountsPage() {
+  return (
+    <div>
+      <AccountsPageComponent />
+    </div>
+  );
+}<AccountsPageComponent />;
 }
